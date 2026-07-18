6c486fd2d5050dbd8ebf3279ff8be19e222f5f3d
Prerit Dayal <prerit714@gmail.com>
2026-07-17 23:19:46 -0400

    chore: tune render-markdown and remove markdown linters

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,6 @@ return {
     config = function()
       local lint = require("lint")
       lint.linters_by_ft = {
-        markdown = { "markdownlint" },
         python = { "pylint" },
         javascript = { "eslint_d" },
         typescript = { "eslint_d" },
